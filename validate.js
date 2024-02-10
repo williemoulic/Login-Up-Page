482db13a3b6795ceee9196035c565b64c30cbba0
williemoulic <willie.moulic@gmail.com>
2024-02-10 23:02:33 +0800

    .

--- validate.js
+++ validate.js
@@ -6,6 +6,9 @@ var password = document.forms['form']['password'];
 var email_error = document.getElementById('email_error');
 var pass_error = document.getElementById('pass_error');
 
+email.addEventListener('textInput', email_Verify);
+password.addEventListener('textInput', pass_Verify);
+
 function validated(){
     if (email.value.length < 9) {
         email.style.border = "1px solid red";
@@ -18,5 +21,22 @@ function validated(){
         pass_error.style.display = "block";
         password.focus();
         return false;
+    }
+    
+}
+
+function email_Verify(){
+    if (email.value.length >= 8) {
+        email.style.border = "1px solid silver";
+        email_error.style.display = "none";
+        return true;       
+    }
+}
+
+function pass_Verify(){
+    if (password.value.length >= 8) {
+        password.style.border = "1px solid silver";
+        pass_error.style.display = "none";
+        return true;       
     }
 }
