8592d3186a960aeb488ae0589a3b5a2fe2908db8
williemoulic <willie.moulic@gmail.com>
2024-02-10 22:16:14 +0800

    Update validate.js

--- validate.js
+++ validate.js
@@ -4,7 +4,7 @@ var email = document.forms['form']['email'];
 var password = document.forms['form']['password'];
 
 var email_error = document.getElementById('email_error');
-var pass_error = document.getElemenentById('pass_error');
+var pass_error = document.getElementById('pass_error');
 
 function validated(){
     if (email.value.length < 9) {
@@ -13,7 +13,7 @@ function validated(){
         email.focus();
         return false;
     }
-    if (password.value.length < 6) {
+    if (password.value.length < 9) {
         password.style.border = "1px solid red";
         pass_error.style.display = "block";
         password.focus();
